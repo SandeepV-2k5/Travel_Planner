f7ce2befe77f43fe1bb2f46e18571bed0cdb9f07
SandeepV-2k5 <143240926+SandeepV-2k5@users.noreply.github.com>
2025-10-29 21:30:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ from datetime import datetime
 
 # Load environment variables
 load_dotenv()
-genai.configure(api_key=os.environ("YOUR_GEMINI_API_KEY"))
+genai.configure(api_key=os.environ["YOUR_GEMINI_API_KEY"])
 
 model = genai.GenerativeModel("gemini-1.5-flash")
 
